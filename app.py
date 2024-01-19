11e9d5285d64c985b434bc05d2757eb65e569777
Hema9121 <hemasrinivasulu.ds@gmail.com>
2024-01-19 19:22:53 +0530

    docker run

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "my first ml project"
+    return "my first ml project-1"
 
 if __name__=="__main__":
     app.run(debug=True)
